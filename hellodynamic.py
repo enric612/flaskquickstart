ef1c64330ac15e0b054feef63a39313ad2715e7d
Enric Climent <enric@ecliment.ovh>
2015-03-04 10:22:13 +0100

    Creades rutes dinamiques amb plantilles

---
+++ hellodynamic.py
@@ -0,0 +1,13 @@
+from flask import Flask
+from flask import render_template
+
+app = Flask(__name__)
+
+#Generem una ruta dinamica on carreguem una platilla.(/templates/hello.html)
+@app.route('/hello/')
+@app.route('/hello/<name>')
+def hello(name=None):
+    return render_template('hello.html', name=name)
+
+if __name__ == '__main__':
+    app.run()
